dc6818fb830488636ee6b91532b91c016485eb91
usnarr <czarny2524@gmail.com>
2024-05-27 20:00:33 +0200

    initial commit for Gamemode

---
+++ web_interface/src/assets/js/gamemode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GameMode(){
+    return (
+        <div>
+            <h2>Select Game Mode</h2>
+            <button>Bullet</button>
+            <button>Rapid</button>
+            <button>Classic</button>
+        </div>
+    )
+}
