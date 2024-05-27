ea3ece1608cdc7357ec49223359cfbfb282d8258
usnarr <czarny2524@gmail.com>
2024-05-27 21:06:55 +0200

    added labels for the buttons and css

--- web_interface/gui/src/assets/js/gamemode.js
+++ web_interface/gui/src/assets/js/gamemode.js
@@ -10,12 +10,15 @@ function GameMode(){
           <h2 class='main-text'>Select Game Mode</h2>
           <div className="icon-buttons">
             <button className="icon-button">
+                <span>Play as White</span> 
                 <img src={whiteCrown} alt="White Crown"/>
             </button>
             <button className="icon-button">
+                <span>Randomize</span> 
                 <img src={halfBlackWhiteCrown} alt="Half Black Half White Crown"/>
             </button>
             <button className="icon-button">
+                <span>Play as Black</span> 
                 <img src={blackCrown} alt="Black Crown"/>
             </button>
           </div>
