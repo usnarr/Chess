8552b7ad896cdf06d9075b088786f268464da0f0
usnarr <czarny2524@gmail.com>
2024-05-27 20:48:56 +0200

    added images as buttons + corresponding css

--- web_interface/gui/src/assets/js/gamemode.js
+++ web_interface/gui/src/assets/js/gamemode.js
@@ -2,19 +2,22 @@ import React from 'react'
 import whiteCrown from '../images/whiteCrown.png'; 
 import halfBlackWhiteCrown from '../images/halfBlackWhiteCrown.png'; 
 import blackCrown from '../images/blackCrown.png'; 
+import '../css/App.css'
 function GameMode(){
     //console.log("gamemode component is rendering");
     return (
         <div>
-          <h2>Select Game Mode</h2>
-          <div onClick={() => console.log('White side selected')}>
-            <img src={whiteCrown} alt="White Crown" style={{ cursor: 'pointer' }} />
-          </div>
-          <div onClick={() => console.log('Half-Black Half-White side selected')}>
-            <img src={halfBlackWhiteCrown} alt="Half Black and Half White Crown" style={{ cursor: 'pointer' }} />
-          </div>
-          <div onClick={() => console.log('Black side selected')}>
-            <img src={blackCrown} alt="Black Crown" style={{ cursor: 'pointer' }} />
+          <h2 id='main-text'>Select Game Mode</h2>
+          <div className="icon-buttons">
+            <button className="icon-button">
+                <img src={whiteCrown} alt="White Crown"/>
+            </button>
+            <button className="icon-button">
+                <img src={halfBlackWhiteCrown} alt="Half Black Half White Crown"/>
+            </button>
+            <button className="icon-button">
+                <img src={blackCrown} alt="Black Crown"/>
+            </button>
           </div>
         </div>
       );
